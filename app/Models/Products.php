4896c191230d5461ac8fa4267b635bc1036bc16f
Omer Mujtaba <omer.mujtaba96@gmail.com>
2024-07-01 17:51:02 +0300

    update Products model to support variation relationship

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -19,4 +19,12 @@ class Products extends Model
     protected $fillable = ['id', 'name', 'sku', 'product_status_id', 'quantity', 'variations', 'price', 'product_currency_id', 'incomplete_import', 'deletion_reason'];
 
     protected $dates = ['deleted_at'];
+
+    /**
+     * Get the variations for the product.
+     */
+    public function variations(): HasMany
+    {
+        return $this->hasMany(ProductVariations::class, 'product_id');
+    }
 }
