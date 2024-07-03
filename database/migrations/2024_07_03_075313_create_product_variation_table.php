bc077e09f46a47a3419dd0ab22fdfa272dbca859
Omer Mujtaba <omer.mujtaba96@gmail.com>
2024-07-03 10:57:16 +0300

    [migration] create product_variation_table

---
+++ database/migrations/2024_07_03_075313_create_product_variation_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variations', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_variation_name');
+            $table->integer('quantity')->nullable()->default(0);
+            $table->string('availability')->nullable();
+            $table->foreignId('product_variation_type_id')->constrained('product_variations_types', 'id')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variation');
+    }
+};
