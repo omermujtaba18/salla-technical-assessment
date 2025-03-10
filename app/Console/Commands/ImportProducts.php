179608584e9c3d5e2475baef445fc53e34f76b69
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-03-10 03:05:04 -0400

    Revert "Update import:products command handler to use jobs"
    This reverts commit e6a45ce26e1d5de6096018f5faf31b4b8218d18b.

--- app/Console/Commands/ImportProducts.php
+++ app/Console/Commands/ImportProducts.php
@@ -2,28 +2,42 @@
 
 namespace App\Console\Commands;
 
-use App\Jobs\ImportProductsFromCSV;
+use App\Models\ProductCurrency;
 use App\Models\Products;
+use App\Models\ProductStatus;
 use Illuminate\Console\Command;
-use Illuminate\Bus\Batch;
-use Illuminate\Http\File;
-use Illuminate\Support\Facades\Bus;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
-use Throwable;
+use App\Rules\ProductValidator;
 
 class ImportProducts extends Command
 {
     /**
      * @var string
      */
-    protected $signature = 'import:products {file : The CSV file to import} {--batchSize=200 : How many rows to process in a batch}';
+    protected $signature = 'import:products {file : The CSV file to import} {--maxRowsToProcess= : The maximum number of rows to process } {--parallelizationEnabled=true : Whether to process parallel}';
 
     /**
      * @var string
      */
     protected $description = 'Imports products into database';
+
+    /**
+     * The ProductValidator instance.
+     *
+     * @var ProductValidator
+     */
+    protected $validator;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param ProductValidator $validator
+     * @return void
+     */
+    public function __construct(ProductValidator $validator)
+    {
+        parent::__construct();
+        $this->validator = $validator;
+    }
 
     /**
      * @return mixed
@@ -33,28 +47,124 @@ class ImportProducts extends Command
         // Get the file as an input 
         $file = $this->argument('file');
 
+        // Added maxRowsToProcess to assist in testing
+        $maxRowsToProcess = (int)$this->option('maxRowsToProcess');
+        $parallelizationEnabled = $this->option('parallelizationEnabled');
+
         // Validate the file
         if (!file_exists($file)) {
             $this->error("File not found: {$file}");
             return;
         }
 
-        $storedFilePath = Storage::putFile('imports', new File($file));
-        $batchSize = $this->option('batchSize');
+        $startTime = microtime(true);
+        $contents = file_get_contents($file);
+        $lines = explode("\n", $contents);
 
-        Bus::batch(new ImportProductsFromCSV($storedFilePath, $batchSize))->then(function (Batch $batch) use ($storedFilePath) {
-            $productIds = Cache::get("productsIds:{$storedFilePath}");
+        $i = 0;
 
-            // Delete products that are not in the imported csv and set their deletion reason to synchronization 
-            if (is_array($productIds) && count($productIds) > 0) {
-                Products::whereNotIn('id', $productIds)->update(['deletion_reason' => 'synchronization', 'deleted_at' => now()->toDateTimeString()]);
-                Cache::forget("productsIds:{$storedFilePath}");
+        foreach ($lines as $line) {
+
+            if ($i % 1000 == 0) {
+                print("Processing... Processed {$i} products \n");
             }
 
-            $executionTime = $batch->finishedAt->diff($batch->createdAt)->format('%H:%I:%S');
-            Log::info("CSV import successful. Execution time: {$executionTime}", ['totalJobs' => $batch->totalJobs]);
-        })->catch(function (Batch $batch, Throwable $e) {
-            // Log::error('CSV import failed', ['error' => $e, 'batch' => $batch, 'totalJobs' => $batch->totalJobs, 'failedJobs' => $batch->failedJobs]);
-        })->name('import:products:batch')->dispatch();
+            // Skip the header row
+            if ($i == 0) {
+                $i++;
+                continue;
+            }
+
+            $fields = explode(",", $line);
+            $fields = str_getcsv($line);
+
+            // Ensure the array has the expected number of elements
+            if (count($fields) < 8) {
+                print("Insufficient columns in CSV line: {$i} \n");
+                $i++;
+                continue;
+
+                // We can also throw an exception in this scenario and ask the user to fix the csv first
+                // throw new Exception("Insufficient columns in CSV line: {$i}");
+            }
+
+            $data = [
+                'id' => $fields[0],
+                'name' => $fields[1],
+                'sku' => $fields[2],
+                'price' => $fields[3],
+                'currency' => $fields[4],
+                'variations' => $fields[5],
+                'quantity' => $fields[6],
+                'status' => $fields[7],
+            ];
+
+            $data = $this->validator->validate($data);
+
+            // SKU is unique so if it exists already set it to null and incomplete_import
+            if ($data['sku'] && Products::withTrashed()->where('sku', $data['sku'])->first()) {
+                $data['sku'] = null;
+                $data['incomplete_import'] = true;
+            }
+
+            $currency = $data['currency'] ? ProductCurrency::firstOrCreate(['currency' => $data['currency']], ['currency' => $data['currency']]) : null;
+            $status = $data['status'] ? ProductStatus::firstOrCreate(['status' => $data['status']], ['status' => $data['status']]) : null;
+
+            $isProductDeleted = $data['status'] === 'deleted';
+
+            $product = Products::withTrashed()->updateOrCreate(['id' => $data['id']],  [
+                'name' => $data['name'],
+                'sku' => $data['sku'],
+                'price' => $data['price'],
+                'product_currency_id' => $currency ? $currency->id : null,
+                'quantity' => $data['quantity'],
+                'product_status_id' => $status ? $status->id : null,
+                'incomplete_import' => $data['incomplete_import'] ?? false,
+                'deletion_reason' => $isProductDeleted ? 'synchronization' : null
+            ]);
+
+            $isProductDeleted ? $product->delete() : $product->restore();
+
+            if (isset($data['variations'])) {
+                $decodedVariations = json_decode("{$data['variations']}", true);
+                if (json_last_error() !== JSON_ERROR_NONE) {
+                    // Mark the product as incomplete_product since variations are invalid
+                    $product->incomplete_import = true;
+                    $product->save();
+                } else {
+                    $product->variation_types()->delete();
+                    foreach ($decodedVariations as $variation) {
+                        if (isset($variation['name'])) {
+                            $productVariationType = $product->variation_types()->create(['variation_type_name' => $variation['name']]);
+                            $values = array_filter(array_map('trim', explode(",", $variation['value'])));
+                            if (isset($values) && is_array($values)) {
+                                foreach ($values as $value) {
+                                    $productVariationType->product_variations()->create(['product_variation_name' => $value]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (isset($maxRowsToProcess) && $i === $maxRowsToProcess) {
+                break;
+            }
+
+            $i++;
+        }
+
+        $startDateTime = (new \DateTime())->setTimestamp($startTime);
+
+        // Delete products that are not in the current file run
+        Products::where('updated_at', '<', $startDateTime)->update(['deletion_reason' => 'synchronization', 'deleted_at' => now()->toDateTimeString()]);
+
+        $endTime = microtime(true);
+        $executionTime = $endTime - $startTime;
+        $formattedTime = gmdate('H:i:s', (int)$executionTime);
+
+        print("Execution Time: {$formattedTime} \n");
+
+        die("Updated {$i} products.\n");
     }
 }
