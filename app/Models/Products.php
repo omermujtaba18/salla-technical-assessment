fe424814701e6e1f26000cffb20aefbe3e0418b7
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-03-10 03:05:04 -0400

    Revert "update Products model"
    This reverts commit 944b63ab1c16451903219997533d1a14018d9f68.

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -4,19 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Products extends Model
 {
     use HasFactory;
-    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['id', 'name', 'sku', 'product_status_id', 'variations', 'price', 'product_currency_id', 'incomplete_import', 'deletion_reason'];
-
-    protected $dates = ['deleted_at'];
+    protected $fillable = ['id', 'name', 'sku', 'product_status_id', 'variations', 'price', 'product_currency_id', 'incomplete_import'];
 }
