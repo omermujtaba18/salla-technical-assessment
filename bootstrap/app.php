4a841df399cfd58910e996ddb8e1a33b9e22c640
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-03-10 03:05:04 -0400

    Revert "add schedule to run at 12am daily"
    This reverts commit 9fb630968229402feb266727a606edce42f97db2.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,20 +1,16 @@
 <?php
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        commands: __DIR__ . '/../routes/console.php',
+        commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->withSchedule(function (Schedule $schedule) {
-        $schedule->command('app:sync-products ServiceA')
-            ->daily()->sendOutputTo('storage/logs/schedule-logs.log');
     })->create();
